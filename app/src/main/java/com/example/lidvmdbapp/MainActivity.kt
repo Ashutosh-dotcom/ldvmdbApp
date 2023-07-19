adc58078a9cbc98aec03b68c91193b15569cfe84
Ashutosh-dotcom <kumar.ashutosh81091@gmail.com>
2023-07-19 15:30:24 +0530

    databinding

---
+++ app/src/main/java/com/example/lidvmdbapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.lidvmdbapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.lidvmdbapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityMainBinding
+    lateinit var viewModel: MainActivityViewModel
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+        viewModel=ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+
+        binding.lifecycleOwner=this
+        binding.myViewModel=viewModel
+
+
+
+
+
+    }
+}
